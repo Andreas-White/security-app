357a518c24a8661bec82cda1083ce414bdbb6594
Andreas Manos <antmanos42@gmail.com>
2021-05-26 00:50:01 +0200

    new dependency in pom.xml for communication with RabbitMQ and implementation of MessagingConfig class for the same reason

---
+++ src/main/java/com/peerlender/securityapp/user/config/MessagingConfig.java
@@ -0,0 +1,34 @@
+package com.peerlender.securityapp.user.config;
+
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+
+public class MessagingConfig {
+
+    private static final String TOPIC = "userRegisteredTopic";
+    private static final String QUEUE_NAME = "user.registered";
+
+    @Bean
+    public Queue userRegisteredQueue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public TopicExchange userRegisteredTopic() {
+        return new TopicExchange(TOPIC);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new ConnectionFactory();
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queue).to(topicExchange).with("user.#");
+    }
+}
