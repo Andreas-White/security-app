ae1797fc9e2f4490a288d9c362f8d8a3ebc36ebc
Andreas Manos <antmanos42@gmail.com>
2021-05-26 00:59:52 +0200

    new dependency in pom.xml and added NotificationService class, also updated UserController

--- src/main/java/com/peerlender/securityapp/application/UserController.java
+++ src/main/java/com/peerlender/securityapp/application/UserController.java
@@ -1,27 +1,34 @@
 package com.peerlender.securityapp.application;
 
 import com.peerlender.securityapp.user.exception.UserNotFoundException;
+import com.peerlender.securityapp.user.model.User;
 import com.peerlender.securityapp.user.repository.UserRepository;
+import com.peerlender.securityapp.user.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     private final UserRepository userRepository;
+    private final NotificationService notificationService;
 
     @Autowired
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, NotificationService notificationService) {
         this.userRepository = userRepository;
+        this.notificationService = notificationService;
     }
 
     @PostMapping(value = "/validate")
     public String validateTokenAndGetUsername(@RequestHeader("Authorization") String token) {
         return userRepository.findById(token).
                 orElseThrow(() -> new UserNotFoundException(token)).getUserName();
+    }
+
+    @PostMapping(value = "/")
+    public void register(@RequestBody User user) {
+        userRepository.save(user);
+        notificationService.sendMessage(user);
     }
 }
---
+++ src/main/java/com/peerlender/securityapp/user/service/NotificationService.java
@@ -0,0 +1,23 @@
+package com.peerlender.securityapp.user.service;
+
+import com.google.gson.Gson;
+import com.peerlender.securityapp.user.model.User;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final static Gson GSON = new Gson();
+
+    @Autowired
+    public NotificationService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(User user) {
+        rabbitTemplate.convertAndSend("userRegisteredTopic","user.registered",GSON.toJson(user));
+    }
+}
